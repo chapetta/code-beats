8cf625051384eb1d7f16d675139c1f8360d1d95a
chapetta <cha.petta@hotmail.com>
2025-09-12 19:06:11 -0300

    feat: adicionado o loading e fazendo a requisição dos albuns

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import searchAlbumsAPI from "../services/searchAlbunsAPI";
+import Loading from "./Loading";
+import type { AlbumType } from "../types";
 
 export default function Search () {
+  const [inputValue, setInputValue ] = useState<string>('')
+  const [isLoading, setIsLoading ] = useState<boolean>(false)
+  const [resultAlbuns, setResultAlbuns ] = useState<AlbumType[]>([])
+
+
+  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value)
+  }
+
+  async function handleButton () {
+    setInputValue('')
+    const result = await searchAlbumsAPI(inputValue)
+    setResultAlbuns(result)
+    setIsLoading(false)
+  }
+
   return (
     <div>
-      <input type="text" placeholder="Nome do Artista" />
-      <button>Procurar</button>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Nome do Artista"
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <button onClick={handleButton}>Procurar</button>
+        </div>
+      )}
     </div>
   );
 }
