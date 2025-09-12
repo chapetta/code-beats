d05d2096d6c1fa39b1dca910d390967c8eabf517
chapetta <cha.petta@hotmail.com>
2025-09-12 18:47:09 -0300

    feat: implementação do loading, navigate, e redirecionamento

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,32 +1,40 @@
 import { createUser } from "../services/userAPI"
 import { useState } from "react"
+import Loading from "./Loading"
+import { useNavigate } from "react-router-dom"
 
 export default function Login() {
   const [inputValue, setInputValue ] = useState<string>('')
+  const [isLoading, setIsLoading ] = useState<boolean>(false)
+
+  const navigate = useNavigate();
 
   function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
 
     setInputValue(e.target.value)
   }
 
-  function handleButton () {
-    createUser({name: inputValue})
-
+  async function handleButton () {
+    setIsLoading(true)
+   await  createUser({name: inputValue})
     setInputValue('')
+    setIsLoading(false)
+    navigate('search')
   }
 
   return (
-  <div>
-    <input
-     type="text" 
-     placeholder="Nome"
-     value={inputValue}
-     onChange={handleChange}
-     />
-    <button
-    onClick={handleButton}
-    >Entrar</button>
-  </div>
-
-)
+    <>
+      {isLoading ? <Loading /> : 
+        <div>
+          <input
+            type="text" 
+            placeholder="Nome"
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <button onClick={handleButton}>Entrar</button>
+        </div>
+      }
+    </>
+  )
 }
