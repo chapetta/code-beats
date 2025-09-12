d9c72a6426ae58c686955666777ef2325fedcdca
chapetta <cha.petta@hotmail.com>
2025-09-12 18:33:58 -0300

    feat: adicionando a funcionalidade do botao e input e testando pra ver se funciona. tudo ok!

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <Login /> } />
+        <Route  path="/" element={ <Login /> } />
       </Routes>
     </>
   )
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,31 @@
-
+import { createUser } from "../services/userAPI"
+import { useState } from "react"
 
 export default function Login() {
+  const [inputValue, setInputValue ] = useState<string>('')
+
+  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
+
+    setInputValue(e.target.value)
+  }
+
+  function handleButton () {
+    createUser({name: inputValue})
+
+    setInputValue('')
+  }
+
   return (
-  
   <div>
-    <input type="text" placeholder="Nome"/>
-    <button>Entrar</button>
+    <input
+     type="text" 
+     placeholder="Nome"
+     value={inputValue}
+     onChange={handleChange}
+     />
+    <button
+    onClick={handleButton}
+    >Entrar</button>
   </div>
 
 )
