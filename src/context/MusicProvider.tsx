fdfa6e07570f9e7e55fed030b6122d17f5d50d2c
chapetta <cha.petta@hotmail.com>
2025-09-13 10:15:47 -0300

    refactory: corrigindo um erro no componente search

--- src/context/MusicProvider.tsx
+++ src/context/MusicProvider.tsx
@@ -1,11 +1,14 @@
+import type { AlbumType } from "../../types";
 import { MusicContext } from "./MusicContext";
-import type { ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
 
 export default function MusicProvider({ children }: { children: ReactNode }) {
 
+  const [resultAlbuns, setResultAlbuns] =  useState<AlbumType[]>([])
+
 
   return (
-    <MusicContext.Provider value={{}}>
+    <MusicContext.Provider value={{ resultAlbuns, setResultAlbuns }}>
       
       {children }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './styles/Global.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import MusicProvider from './context/MusicProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <MusicProvider> 
+        <App />
+      </MusicProvider>
     </BrowserRouter>
   </StrictMode>,
 )
