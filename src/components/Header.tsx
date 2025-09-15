5f7363df03abbb4231c0b67b61397f555a1e706a
chapetta <cha.petta@hotmail.com>
2025-09-15 19:28:22 -0300

    feat: criando o componente header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export function Header () {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Pesquisar</li>
+          <li>Favoritas</li>
+          <li>Perfil</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
