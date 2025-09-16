6bcc52986605faf2a42c9fe783132695e2eaf3ae
chapetta <cha.petta@hotmail.com>
2025-09-16 07:30:46 -0300

    feat: adicionando o checkbox de favorite

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,11 +1,25 @@
 import type { SongType } from "../../types"
+import { useState } from "react"
+import checkedHeart from "../assets/checked_heart.png"
+import emptyHeart from "../assets/empty_heart.png"
 
 type MusicCardProps = {
   music: SongType,
 }
 
 export function MusicCard ({music}: MusicCardProps) {
+  const [ isFavorite, setIsFavorite ] = useState<boolean>(false)
+  const [imgChechked, setImgChecked] = useState<string>("")
 
+    function handleCheckBox () {
+      if (!isFavorite) {
+        setImgChecked("../assets/checked_heart.png")
+        setIsFavorite(true)
+      } else {
+        setImgChecked("../assets/empty_heart.png")
+        setIsFavorite(false)
+      }
+    }
   return (
     <div>
       <p>{music.trackName}</p>
@@ -13,6 +27,18 @@ export function MusicCard ({music}: MusicCardProps) {
       <track kind="captions" />
       O seu navegador não suporta o elemento{" "} <code>audio</code>.
       </audio>
+      <label>
+      <input type="checkbox" 
+      onChange={handleCheckBox}
+      checked={isFavorite}
+      />
+      <img 
+      src={isFavorite ? checkedHeart : emptyHeart} 
+      alt="favorite" 
+      
+      
+      />
+      </label>
     </div>
   )
 }
